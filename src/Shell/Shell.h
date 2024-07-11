3af55b9234c581c255326befad7b89f99b1b4a74
Inplex-sys <69421356+Inplex-sys@users.noreply.github.com>
2024-07-11 08:27:42 +0200

    Adding Registery & ICommand to prototypes

--- src/Shell/Shell.h
+++ src/Shell/Shell.h
@@ -1,20 +1,29 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <map>
+#include <memory>
+#include <sstream>
 #include <string>
+#include <memory>
+#include <vector>
+#include <iostream>
 
+class ICommand;
 class Terminal;
 
-class Shell {
-    private:
-        std::string prompt;
+class Shell
+{
+private:
+    std::map<std::string, std::shared_ptr<ICommand>> commandRegistry;
+    std::string prompt;
 
-    public:
-        Terminal* terminal;
+public:
+    Terminal *terminal;
 
-        Shell();
-        void Interpret(std::string command);
-        void Prompt();
+    Shell();
+    void Interpret(const std::string &input);
+    void Prompt();
 };
 
 #endif
