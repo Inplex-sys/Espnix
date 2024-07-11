1bea9ce4c9771adf696a4cc8d4b1883a27c5c3a8
Inplex-sys <69421356+Inplex-sys@users.noreply.github.com>
2024-07-11 08:27:06 +0200

    Update Shell.cpp

--- src/Shell/Shell.cpp
+++ src/Shell/Shell.cpp
@@ -1,5 +1,5 @@
-#include <string>
 #include <iostream>
+#include <string>
 #include <map>
 #include <memory>
 #include <sstream>
@@ -9,59 +9,66 @@
 
 #include "Shell.h"
 
-class ICommand {
+class ICommand
+{
 public:
     virtual ~ICommand() = default;
-    virtual void Execute(const std::vector<std::string>& args, Terminal* terminal) = 0;
+    virtual void Execute(const std::vector<std::string> &args, Terminal *terminal) = 0;
 };
 
-class ListCommand : public ICommand {
+class ListCommand : public ICommand
+{
 public:
-    void Execute(const std::vector<std::string>& args, Terminal* terminal) override {
+    void Execute(const std::vector<std::string> &args, Terminal *terminal) override
+    {
         terminal->Write("Executing 'ls' command\n");
     }
 };
 
-class ChangeDirectoryCommand : public ICommand {
+class ChangeDirectoryCommand : public ICommand
+{
 public:
-    void Execute(const std::vector<std::string>& args, Terminal* terminal) override {
+    void Execute(const std::vector<std::string> &args, Terminal *terminal) override
+    {
         terminal->Write("Executing 'cd' command\n");
     }
 };
 
-class Shell {
-private:
-    std::map<std::string, std::shared_ptr<ICommand>> commandRegistry;
-    std::string prompt;
+Shell::Shell() : terminal(nullptr)
+{
+    commandRegistry["ls"] = std::make_shared<ListCommand>();
+    commandRegistry["cd"] = std::make_shared<ChangeDirectoryCommand>();
+    this->prompt = "espnix:~$ ";
+}
 
-public:
-    Terminal *terminal;
-
-    Shell() : terminal(nullptr) {
-        commandRegistry["ls"] = std::make_shared<ListCommand>();
-        commandRegistry["cd"] = std::make_shared<ChangeDirectoryCommand>();
-        this->prompt = "espnix:~$ ";
+void Shell::Interpret(const std::string &input)
+{
+    std::istringstream iss(input);
+    std::string command;
+    std::vector<std::string> args;
+    iss >> command;
+    std::string arg;
+    while (iss >> arg)
+    {
+        args.push_back(arg);
     }
 
-    void Interpret(const std::string& input) {
-        std::istringstream iss(input);
-        std::string command;
-        std::vector<std::string> args;
-        iss >> command;
-        std::string arg;
-        while (iss >> arg) {
-            args.push_back(arg);
-        }
+    terminal->Write(command + "\n");
 
-        auto it = commandRegistry.find(command);
-        if (it != commandRegistry.end()) {
-            it->second->Execute(args, terminal);
-        } else {
-            terminal->Write("Command not found\n");
-        }
+    auto it = commandRegistry.find(command);
+    if (it != commandRegistry.end())
+    {
+        it->second->Execute(args, terminal);
+    }
+    else
+    {
+        terminal->Write(command + ": command not found\n");
     }
 
-    void Prompt() {
-        terminal->Write(prompt);
-    }
-};
+    this->Prompt();
+}
+
+void Shell::Prompt()
+{
+    terminal->Write(prompt);
+}
